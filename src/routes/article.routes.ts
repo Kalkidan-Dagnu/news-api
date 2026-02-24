0701ffa212698e641b1a1ddf7ad856521680c0a8
Kal D <kalajanegassykoki@gmail.com>
2026-02-24 09:48:10 +0300

    feat: implement paginated author content list (/articles/me)

--- src/routes/article.routes.ts
+++ src/routes/article.routes.ts
@@ -1,15 +1,22 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/auth.middleware";
 import { authorize } from "../middleware/rbac.middleware";
-import { createArticle } from "../controllers/article.controller";
+import { createArticle, getMyArticles } from "../controllers/article.controller";
 
 const articleRouter = Router();
 
 articleRouter.post(
-  "/",
-  authenticate,
-  authorize("author"),
-  createArticle
+    "/",
+    authenticate,
+    authorize("author"),
+    createArticle
+);
+
+articleRouter.get(
+    "/me", 
+    authenticate, 
+    authorize("author"), 
+    getMyArticles
 );
 
 export default articleRouter;
